f68f65e405e4a5884c67482c4f174184fe203a72
Alexandre Annic <alexandre.annic1@gmail.com>
2022-01-13 15:29:58 +0100

    Continue
    Continue
    
    Continue

--- src/client/report/ReportDraft.ts
+++ src/client/report/ReportDraft.ts
@@ -33,14 +33,15 @@ export interface ReportDraft {
   vendor: string
   ccrfCode?: string[]
   reponseconsoCode?: string[]
-  ccrfCode?: string[]
   tags?: ReportTag[]
-  contractualDispute?: boolean;
+  contractualDispute?: boolean
+  forwardToReponseConso?: boolean
+  companyKind?: CompanyKinds
 }
 
 export class ReportDraft {
-  static readonly getCompanyKind = (r: ReportDraft): CompanyKinds => {
-    return ReportDraft.getLastSubcategory(r)?.companyKind ?? CompanyKinds.SIRET
+  static readonly getCompanyKindFomSubcategories = (r: ReportDraft): CompanyKinds | undefined => {
+    return r.subcategories?.reverse().find(_ => !!_.companyKind)?.companyKind
   }
 
   static readonly getLastSubcategory = (r: ReportDraft): Subcategory | undefined => {
@@ -59,7 +60,7 @@ export class ReportDraft {
 
   static readonly tags = (r: ReportDraft): ReportTag[] => {
     const tags = (r.subcategories ?? []).flatMap(_ => _.tags ?? [])
-    if (ReportDraft.companyKind(r) === CompanyKinds.WEBSITE) {
+    if (ReportDraft.getCompanyKindFomSubcategories(r) === CompanyKinds.WEBSITE) {
       tags.push(ReportTag.Internet)
     }
     if (!r.forwardToReponseConso) {
@@ -80,13 +81,67 @@ export class ReportDraft {
     return !!r.tags && r.tags.includes(ReportTag.Influenceur)
   }
 
-  static readonly isTransmittableToPro = (r: ReportDraft): boolean => {
+  static readonly isTransmittableToPro = (r: Pick<ReportDraft, 'employeeConsumer' | 'tags'>): boolean => {
     return !r.employeeConsumer
       && !(r.tags ?? []).find(_ => ([
         ReportTag.ReponseConso,
         ReportTag.ProduitDangereux,
-        ReportTag.Bloctel]
-      ).includes(_))
+        ReportTag.Bloctel
+      ]).includes(_))
+  }
+
+  static readonly isGovernmentCompany = (_?: {activityCode?: string}): boolean => _?.activityCode?.startsWith('84.') ?? false
+}
+
+export class ReportDraft_ {
+  static readonly getCompanyKindFomSubcategories = (r: ReportDraft): CompanyKinds | undefined => {
+    return r.subcategories?.reverse().find(_ => !!_.companyKind)?.companyKind
+  }
+
+  static readonly getLastSubcategory = (r: ReportDraft): Subcategory | undefined => {
+    if (r.subcategories && r.subcategories.length) {
+      return r.subcategories[r.subcategories.length - 1]
+    }
+  }
+
+  static readonly getReponseconsoCode = (r: ReportDraft): string[] => {
+    return uniqby((r.subcategories ?? []).flatMap(_ => _.reponseconsoCode ?? []), _ => _)
+  }
+
+  static readonly ccrfCode = (r: ReportDraft): string[] => {
+    return uniqby((r.subcategories ?? []).flatMap(_ => _.ccrfCode ?? []), _ => _)
+  }
+
+  static readonly tags = (r: ReportDraft): ReportTag[] => {
+    const tags = (r.subcategories ?? []).flatMap(_ => _.tags ?? [])
+    if (ReportDraft.getCompanyKindFomSubcategories(r) === CompanyKinds.WEBSITE) {
+      tags.push(ReportTag.Internet)
+    }
+    if (!r.forwardToReponseConso) {
+      return tags.filter(_ => _ !== ReportTag.ReponseConso)
+    }
+    return tags
+  }
+
+  static readonly isContractualDispute = (r: ReportDraft): boolean => {
+    return !r.employeeConsumer && !!r.tags && r.tags.includes(ReportTag.LitigeContractuel)
+  }
+
+  static readonly isVendor = (r: ReportDraft): boolean => {
+    return !!r.tags && r.tags.includes(ReportTag.ProduitDangereux)
+  }
+
+  static readonly isInfluenceur = (r: ReportDraft): boolean => {
+    return !!r.tags && r.tags.includes(ReportTag.Influenceur)
+  }
+
+  static readonly isTransmittableToPro = (r: Pick<ReportDraft, 'employeeConsumer' | 'tags'>): boolean => {
+    return !r.employeeConsumer
+      && !(r.tags ?? []).find(_ => ([
+        ReportTag.ReponseConso,
+        ReportTag.ProduitDangereux,
+        ReportTag.Bloctel
+      ]).includes(_))
   }
 
   static readonly isGovernmentCompany = (_?: {activityCode?: string}): boolean => _?.activityCode?.startsWith('84.') ?? false
