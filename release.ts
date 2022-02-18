8e0798b0b6521325d05e3293db31d0b23bb23093
Alexandre Annic <alexandre.annic1@gmail.com>
2022-02-18 10:50:36 +0100

    test
    Draft
    
    
    draft
    
    
    draft
    
    
    draft
    
    
    draft
    
    
    draft
    
    
    test
    
    
    test

--- release.ts
+++ release.ts
@@ -1,8 +1,47 @@
-import {execSync} from 'child_process'
+import {exec, execSync} from 'child_process'
+import * as fs from 'fs'
 
-const branch = execSync('git branch --show-current').toString()
-if (branch === 'main') {
-  console.log('ok')
-} else {
-  console.log('nok')
+const config = {
+  mainBranch: 'main',
 }
+
+const run = (cl: string) => {
+  return new Promise((resolve, reject) => {
+    const _ = exec(cl)
+    _.stdout?.on('data', console.log)
+    _.stderr?.on('data', console.error)
+    _.on('exit', code => {
+      if (code === 0) {
+        resolve(code)
+      } else {
+        console.log(`${cl} exited with ${code}`)
+        reject(code)
+      }
+    })
+  })
+}
+const gitWorkspaceIsEmpty = () => execSync(`git status --porcelain`).toString() === ''
+
+const newversion = process.argv[2] ?? 'patch' as 'patch' | 'minor' | 'major'
+
+const getPackageVersion = () => JSON.parse(fs.readFileSync('package.json', 'utf8')).version
+
+const isOnMainBranch = () => /main\s*\n*/.test(execSync('git branch --show-current').toString())
+
+;(async () => {
+  if (!isOnMainBranch()) {
+    console.error(`You must be on branch ${config.mainBranch} to publish.`)
+  } else if (!gitWorkspaceIsEmpty()) {
+    console.error(`Your git status must be clean before to publish.`)
+  } else {
+    await run(`npm run build`)
+    await run(`git add -A`)
+    await run(`npm version ${newversion}`)
+    await run(`npm publish`)
+    await run(`git commit -m "Release ${getPackageVersion()}"`)
+    await run(`git push`)
+    await run(`git checkout prod`)
+    await run(`git merge main`)
+    console.log('ok')
+  }
+})()
