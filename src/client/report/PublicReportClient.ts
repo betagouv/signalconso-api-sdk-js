c8ded9b8d95057b18b5b85056a0bbee88c4d44c3
Alexandre Annic <alexandre.annic1@gmail.com>
2022-03-14 22:53:19 +0100

    Add create endpoint

--- src/client/report/PublicReportClient.ts
+++ src/client/report/PublicReportClient.ts
@@ -1,10 +1,15 @@
-import {Id, ReviewOnReportResponse} from '../../model'
+import {Id, Report, ReviewOnReportResponse} from '../../model'
 import {ApiClientApi} from '../../core/ApiClient'
+import {ReportDraft} from './ReportDraft'
 
 export class PublicReportClient {
   constructor(private client: ApiClientApi) {}
 
   readonly postReviewOnReportResponse = (reportId: Id, review: ReviewOnReportResponse) => {
     return this.client.post<void>(`/reports/${reportId}/response/review`, {body: review})
+  }
+
+  readonly create = (draft: ReportDraft) => {
+    return this.client.post<Report>(`/reports`, {body: ReportDraft.toApi(draft)})
   }
 }
