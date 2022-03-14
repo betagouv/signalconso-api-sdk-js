1934fac744e83b2ad392cf2a2c44eda17a8dfed9
Alexandre Annic <alexandre.annic1@gmail.com>
2022-03-14 23:06:20 +0100

    Clean

--- lib/client/report/PublicReportClient.d.ts
+++ lib/client/report/PublicReportClient.d.ts
@@ -1,7 +1,9 @@
-import { Id, ReviewOnReportResponse } from '../../model';
+import { Id, Report, ReviewOnReportResponse } from '../../model';
 import { ApiClientApi } from '../../core/ApiClient';
+import { ReportDraft } from './ReportDraft';
 export declare class PublicReportClient {
     private client;
     constructor(client: ApiClientApi);
     readonly postReviewOnReportResponse: (reportId: Id, review: ReviewOnReportResponse) => Promise<void>;
+    readonly create: (draft: ReportDraft) => Promise<Report>;
 }
