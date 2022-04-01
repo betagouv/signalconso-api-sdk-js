7d3a450b9dfd967a5abc4d2c4b0e45cb3a40c1f1
Alexandre Annic <alexandre.annic1@gmail.com>
2022-04-01 11:55:15 +0200

    api error

--- lib/core/ApiClient.d.ts
+++ lib/core/ApiClient.d.ts
@@ -28,13 +28,13 @@ export declare type StatusCode = 'front-side' | 200 | 301 | 302 | 400 | 401 | 40
 export interface ApiErrorDetails {
     code: StatusCode;
     id?: string;
-    error?: Error;
     request: {
         method: Method;
         url: string;
         qs?: any;
         body?: any;
     };
+    error?: Error;
 }
 export declare class ApiError extends Error {
     message: string;
