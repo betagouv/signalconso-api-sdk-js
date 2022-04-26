bef0f67b3bbfe6bd6302f69fcf77e5c341d811c9
ssedoudbgouv <s.sedoud.betagouv@gmail.com>
2022-04-26 15:31:47 +0200

    TRELLO-1201: Fix internet tag

--- lib/client/report/PublicReportClient.d.ts
+++
@@ -1,9 +0,0 @@
-import { Id, Report, ResponseConsumerReview } from '../../model';
-import { ApiClientApi } from '../../core/ApiClient';
-import { ReportDraft } from './ReportDraft';
-export declare class PublicReportClient {
-    private client;
-    constructor(client: ApiClientApi);
-    readonly postReviewOnReportResponse: (reportId: Id, review: ResponseConsumerReview) => Promise<void>;
-    readonly create: (draft: ReportDraft) => Promise<Report>;
-}
