c207a717e062c25f53f5f87e8b25e4913069ff65
Alexandre Annic <alexandre.annic1@gmail.com>
2022-05-30 14:58:49 +0100

    Clean

--- script-to-test-github-actions.ts
+++
@@ -1,4 +0,0 @@
-const newversion = process.argv[2] ?? 'patch' as 'patch' | 'minor' | 'major'
-console.log('--------------------------------------------------')
-console.log('STARTEDDDD', newversion)
-console.log('--------------------------------------------------')
